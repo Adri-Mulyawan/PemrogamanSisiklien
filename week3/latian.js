7b8eb89d626ae6572a9b2afb8fcd4710b6383194
Adri-Mulyawan <crujer78@gmail.com>
2026-04-02 16:33:10 +0700

    first commit

---
+++ week3/latian.js
@@ -0,0 +1,17 @@
+// object
+const mahasiswa = {
+    nim: "14926",
+    nama: "Adri Mulyawan"
+    status: true,
+    matkul: [
+        {
+            matkulId: "MK001",
+            nilai: 89
+        },
+        {
+            matkulId: "MK032",
+        }
+    ]
+};
+
+console.log(mahasiswa);
